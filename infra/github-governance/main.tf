8eecea8a5e386c6675c061e810b3d951583f7b5b
vamsikrishna <vamsikrishna2049@gmail.com>
2026-05-23 19:23:38 +0530

    Aligned

--- infra/github-governance/main.tf
+++ infra/github-governance/main.tf
@@ -5,12 +5,12 @@ provider "github" {
 module "github_governance" {
   source = "../modules/github-governance"
 
-  github_owner                         = var.github_owner
-  github_repo                          = var.github_repo
-  manager_github_username              = var.manager_github_username
-  protected_branch                     = var.protected_branch
-  require_code_owner_reviews           = var.require_code_owner_reviews
-  required_status_check_contexts       = var.required_status_check_contexts
-  production_wait_timer_minutes        = var.production_wait_timer_minutes
+  github_owner                          = var.github_owner
+  github_repo                           = var.github_repo
+  manager_github_username               = var.manager_github_username
+  protected_branch                      = var.protected_branch
+  require_code_owner_reviews            = var.require_code_owner_reviews
+  required_status_check_contexts        = var.required_status_check_contexts
+  production_wait_timer_minutes         = var.production_wait_timer_minutes
   require_production_environment_review = var.require_production_environment_review
 }
